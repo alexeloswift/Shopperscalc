dadf05b536a7ea3cd5e80487d82c16f85f8aa206
Alex Diaz <alexelo.swift@gmail.com>
2022-03-31 23:40:31 -0400

    Initial Commit

---
+++ Shopperscalc/ShopperscalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopperscalcApp.swift
+//  Shopperscalc
+//
+//  Created by Alexis Diaz on 3/31/22.
+//
+
+import SwiftUI
+
+@main
+struct ShopperscalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
